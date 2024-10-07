effd3a75009a5d4387e4865b2208ddede28354df
Astekin34 <isadogruca@hotmail.com>
2024-10-07 10:59:18 +0300

    Update Bolum19.cs

--- Bolum19.cs
+++ Bolum19.cs
@@ -1,41 +1,73 @@
-var p1 = new Person("Scott", "Hansel",new DateOnly(1970,1,1));
-var p2 = new Person("David", "Howler",new DateOnly(1986,1,1));
+using System;
+using System.Collections.Generic;
+
+Console.WriteLine("Hello OOP");
+var p1 = new Person("Scott", "Hansel", new DateOnly(1970, 1, 1));
+var p2 = new Person("David", "Howler", new DateOnly(1986, 1, 1));
 
 p1.Pets.Add(new Dog("Fred"));
 p1.Pets.Add(new Dog("Barney"));
 
 p2.Pets.Add(new Cat("Beyonce"));
 
-List<Person>() people = [p1, p2];
+List<Person> people = new List<Person> { p1, p2 };
 foreach (var person in people)
-{        
+{
     Console.WriteLine($"{person}");
     foreach (var pet in person.Pets)
     {
-        Console.WriteLine($"    {pet}");
+        Console.WriteLine($"    {pet} says: {pet.MakeNoise()}");
     }
 }
-public class Person(string firstname, string lastname, DateOnly birthday)
+
+public class Person
 {
-    public string First { get; } = firstname;
-    public string Last { get; } = lastname;
-    public Dateonly Birthday { get; } = birthday;
+    public string First { get; }
+    public string Last { get; }
+    public DateOnly Birthday { get; }
 
     public List<Pet> Pets { get; } = new();
+
+    public Person(string firstname, string lastname, DateOnly birthday)
+    {
+        First = firstname;
+        Last = lastname;
+        Birthday = birthday;
+    }
+
+    public override string ToString()
+    {
+        return $"Human {First}, {Last}";
+    }
 }
 
-public abstract class Pet(string firstname)
+public abstract class Pet
 {
-    public string first { get; } = firstname;
+    public string First { get; }
+
+    protected Pet(string firstname)
+    {
+        First = firstname;
+    }
 
     public abstract string MakeNoise();
-}
-public class Cat(string firstname) : Pet(firstname)
-{
-    public string MakeNoise() => "meow";
+
+    public override string ToString()
+    {
+        return $"{First}, and I am a {GetType().Name}";
+    }
 }
 
-public class Dog(string firstname) : Pet(firstname)
+public class Cat : Pet
 {
-    public string MakeNoise() => "bark";
-}  
+    public Cat(string firstname) : base(firstname) {}
+
+    public override string MakeNoise() => "meow";
+}
+
+public class Dog : Pet
+{
+    public Dog(string firstname) : base(firstname) {}
+
+    public override string MakeNoise() => "bark";
+}
